4f77827615443e0e2abd9312ac378d59c203b15c
Martin gamil <150841633+maro2024@users.noreply.github.com>
2024-08-07 04:42:28 +0300

    Update form.js

--- form.js
+++ form.js
@@ -13,9 +13,9 @@ var iconaia= document.getElementById("iconaia");
 iconaia.onclick =  function(){
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")){
-        iconaia.src = "image/sun1.png";
+        iconaia.src = "sun1.png";
     }else{
-        iconaia.src = "image/moon2.png";
+        iconaia.src = "moon2.png";
     }
   
   
